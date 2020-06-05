45ca9c0100291dd795b2c19ac1c45dc852596df2
Christophe <swolfschristophe@gmail.com>
2020-06-05 20:07:34 +0200

    removed old verts list

--- addon_add_object_drawer.py
+++ addon_add_object_drawer.py
@@ -1,3 +1,8 @@
+from mathutils import Vector
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from bpy.props import FloatVectorProperty
+from bpy.types import Operator
+import bpy
 bl_info = {
     "name": "New Object",
     "author": "Your Name Here",
@@ -11,13 +16,6 @@ bl_info = {
 }
 
 
-import bpy
-from bpy.types import Operator
-from bpy.props import FloatVectorProperty
-from bpy_extras.object_utils import AddObjectHelper, object_data_add
-from mathutils import Vector
-
-
 def add_object(self, context):
     scale_x = self.scale.x
     scale_y = self.scale.y
@@ -25,30 +23,16 @@ def add_object(self, context):
     bottom_thickness = self.bottom_thickness
     top_thickness = self.top_thickness
     has_top = self.has_top
-    
 
-#    verts = [
-#        #bottom
-#        Vector((-1 * scale_x/2, 1 * scale_y/2, 0)),
-#        Vector((1 * scale_x/2, 1 * scale_y/2, 0)),
-#        Vector((1 * scale_x/2, -1 * scale_y/2, 0)),
-#        Vector((-1 * scale_x/2, -1 * scale_y/2, 0)),
-#        
-#        #back
-#        Vector((-1 * scale_x/2, 1 * scale_y/2 - my_float , my_float)),
-#        Vector((1 * scale_x/2, 1 * scale_y/2 - my_float, my_float)),
-#        Vector((-1 * scale_x/2, 1 * scale_y/2 - my_float, 1 + my_float)),
-#        Vector((1 * scale_x/2, 1 * scale_y/2 - my_float, 1 + my_float)),
-#    ]
 
     verts = [
-        #bottom
+        # bottom
         Vector((-1 * scale_x/2, 1 * scale_y/2, 0)),
         Vector((1 * scale_x/2, 1 * scale_y/2, 0)),
         Vector((1 * scale_x/2, -1 * scale_y/2, 0)),
         Vector((-1 * scale_x/2, -1 * scale_y/2, 0)),
 
-        #top    
+        # top
         Vector((-1 * scale_x/2, 1 * scale_y/2, bottom_thickness)),
         Vector((1 * scale_x/2, 1 * scale_y/2, bottom_thickness)),
         Vector((1 * scale_x/2, -1 * scale_y/2, bottom_thickness)),
@@ -56,22 +40,25 @@ def add_object(self, context):
 
 
 
-          #bottom
+        # bottom
         Vector((-1 * scale_x/2, 1 * scale_y/2, bottom_thickness + scale_z)),
         Vector((1 * scale_x/2, 1 * scale_y/2, bottom_thickness + scale_z)),
         Vector((1 * scale_x/2, -1 * scale_y/2, bottom_thickness + scale_z)),
         Vector((-1 * scale_x/2, -1 * scale_y/2, bottom_thickness + scale_z)),
 
-        #top    
-        Vector((-1 * scale_x/2, 1 * scale_y/2, bottom_thickness + top_thickness + scale_z)),
-        Vector((1 * scale_x/2, 1 * scale_y/2, bottom_thickness + top_thickness + scale_z)),
-        Vector((1 * scale_x/2, -1 * scale_y/2, bottom_thickness + top_thickness + scale_z)),
-        Vector((-1 * scale_x/2, -1 * scale_y/2, bottom_thickness + top_thickness + scale_z)),
+        # top
+        Vector((-1 * scale_x/2, 1 * scale_y/2,
+                bottom_thickness + top_thickness + scale_z)),
+        Vector((1 * scale_x/2, 1 * scale_y/2,
+                bottom_thickness + top_thickness + scale_z)),
+        Vector((1 * scale_x/2, -1 * scale_y/2,
+                bottom_thickness + top_thickness + scale_z)),
+        Vector((-1 * scale_x/2, -1 * scale_y/2,
+                bottom_thickness + top_thickness + scale_z)),
     ]
-    
+
     for vert in verts:
         vert.z += 0
-        
 
     edges = []
 
@@ -91,31 +78,25 @@ def add_object(self, context):
         [6, 5, 1, 2]
     ]
 
-
     indexRange = len(faces_template) + 6 if has_top else len(faces_template)
-    for x in range(0, indexRange ):
+    for x in range(0, indexRange):
         repeatingIndex = faces_template[x % len(faces_template)]
         loopCounter = x // len(faces_template)
         offset = loopCounter * 8
 
-
-        offset_vector = [offset,offset,offset,offset]
-        newVector = [repeatingIndex[i]+offset_vector[i] for i in range(len(repeatingIndex))]
+        offset_vector = [offset, offset, offset, offset]
+        newVector = [repeatingIndex[i]+offset_vector[i]
+                     for i in range(len(repeatingIndex))]
         faces.append(newVector)
-
-    
 
     mesh = bpy.data.meshes.new(name="New Object Mesh")
     mesh.from_pydata(verts, edges, faces)
     # useful for development when the mesh may be invalid.
     # mesh.validate(verbose=True)
     newObject = object_data_add(context, mesh, operator=self)
-    
+
 #    bpy.ops.object.modifier_add(type='SOLIDIFY')
 #    bpy.context.object.modifiers["Solidify"].thickness = my_float
-
-
-
 
 
 class OBJECT_OT_add_object(Operator, AddObjectHelper):
@@ -130,7 +111,7 @@ class OBJECT_OT_add_object(Operator, AddObjectHelper):
         subtype='TRANSLATION',
         description="scaling",
     )
-    
+
     bottom_thickness: bpy.props.FloatProperty(
         name="Bottom thickness",
         unit='LENGTH',
@@ -145,9 +126,7 @@ class OBJECT_OT_add_object(Operator, AddObjectHelper):
 
     has_top:  bpy.props.BoolProperty(
         name="Top"
-    ) 
-
-
+    )
 
     def execute(self, context):
 
